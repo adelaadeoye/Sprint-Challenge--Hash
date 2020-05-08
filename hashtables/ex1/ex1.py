ce66f9d266b0a5198e9e8b15335f8fc0f8dfccc7
adeoye adela <adeoye.adela@gmail.com>
2020-05-08 17:37:16 +0100

    ex4 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,12 @@
 def get_indices_of_item_weights(weights, length, limit):
-
     """
     YOUR CODE HERE
     """
+    hashTable={}
+    for i in range(length):
+        hashTable[i]=weights[i]
+        
+    print(hashTable)
+
 
     return None
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,16 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-
+    cache={}
+    result=[]
+    for element in a:
+        if element not in cache:
+            cache[element]=element
+    
+    for element in cache:
+        if element > 0 and element* (-1) in cache:
+            result.append(element)
+    # print(result)
     return result
 
 
