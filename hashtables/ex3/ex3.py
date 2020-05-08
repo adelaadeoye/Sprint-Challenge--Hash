45ec7d22c26f465643fc50ccbf5550ceabf0a5f0
adeoye adela <adeoye.adela@gmail.com>
2020-05-08 19:35:37 +0100

    ex4 solve

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,3 +1,5 @@
+import collections
+
 def intersection(arrays):
 
     """
@@ -7,12 +9,10 @@ def intersection(arrays):
     result=[]
     for element in range(len(arrays)):
         cache[element]=arrays[element]
-    # print(cache)
     for i in range(len(cache)):
-        result.append(cache[i])
-    return list(result)
-
-
+        result+=(cache[i])
+    result=[item for item, count in collections.Counter(result).items() if count > 1]
+    return(result)
 if __name__ == "__main__":
     arrays = []
 
